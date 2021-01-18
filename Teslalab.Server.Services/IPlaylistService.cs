14f3f126954b5819bde6628801762b8aa9fa2c38
$udiSimba <sudisimbadav@gmail.com>
2021-01-18 21:24:07 +0200

    Completed Playlist CRUD operations and pagination

--- Teslalab.Server.Services/IPlaylistService.cs
+++ Teslalab.Server.Services/IPlaylistService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Teslalab.Repositories;
 using Teslalab.Server.Infrastructure;
+using Teslalab.Server.Models.Mappers;
 using Teslalab.Server.Models.Models;
 using Teslalab.Shared;
 using Teslalab.Shared.DTOs;
@@ -18,7 +20,7 @@ namespace Teslalab.Server.Services
 
         Task<OperationResponse<PlaylistDto>> RemoveAsync(string id);
 
-        //Task<OperationResponse<PlaylistDto>> (PlaylistDto model);
+        CollectionResponse<PlaylistDto> GetAllPlaylists(int pageNumber = 1, int pageSize = 10);
     }
 
     public class PlaylistService : IPlaylistService
@@ -53,14 +55,89 @@ namespace Teslalab.Server.Services
             };
         }
 
-        public Task<OperationResponse<PlaylistDto>> RemoveAsync(string id)
+        public CollectionResponse<PlaylistDto> GetAllPlaylists(int pageNumber = 1, int pageSize = 10)
         {
-            throw new NotImplementedException();
+            //Validation
+
+            if (pageNumber < 1)
+                pageNumber = 1;
+
+            if (pageSize < 5)
+                pageSize = 5;
+
+            if (pageSize > 50)
+                pageSize = 50;
+
+            var playlists = _unitOfWork.Playlists.GetAll();
+
+            int playlistCount = playlists.Count();
+
+            var playlistsInRange = playlists
+                        .Skip((pageNumber - 1) * pageSize)
+                        .Take(pageSize)
+                        .Select(p => p.ToPlaylistDto());
+
+            int pagesCount = playlistCount / pageSize;
+            if ((playlistCount & pageSize) != 0)
+                pagesCount++;
+
+            return new CollectionResponse<PlaylistDto>
+            {
+                IsSuccess = true,
+                Message = "Playlists retrieved successfully!",
+                Records = playlistsInRange.ToArray(),
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                PagesCount = pagesCount
+            };
         }
 
-        public Task<OperationResponse<PlaylistDto>> UpdateAsync(PlaylistDto model)
+        public async Task<OperationResponse<PlaylistDto>> RemoveAsync(string id)
         {
-            throw new NotImplementedException();
+            var playlist = await _unitOfWork.Playlists.GetPlaylistById(id);
+
+            if (playlist == null)
+                return new OperationResponse<PlaylistDto>
+                {
+                    IsSuccess = false,
+                    Data = null,
+                    Message = "Playlist not found"
+                };
+
+            _unitOfWork.Playlists.Remove(playlist);
+
+            await _unitOfWork.CommitChangesAsync(_identity.UserId);
+
+            return new OperationResponse<PlaylistDto>
+            {
+                IsSuccess = true,
+                Message = "Playlist has been deleted successfully!",
+                Data = playlist.ToPlaylistDto()
+            };
+        }
+
+        public async Task<OperationResponse<PlaylistDto>> UpdateAsync(PlaylistDto model)
+        {
+            var playlist = await _unitOfWork.Playlists.GetPlaylistById(model.Id);
+
+            if (playlist == null)
+                return new OperationResponse<PlaylistDto>
+                {
+                    IsSuccess = false,
+                    Data = null,
+                    Message = "Playlist not found"
+                };
+            playlist.Name = model.Name;
+            playlist.Description = model.Description;
+
+            await _unitOfWork.CommitChangesAsync(_identity.UserId);
+
+            return new OperationResponse<PlaylistDto>
+            {
+                IsSuccess = true,
+                Message = "Playlist has been updated successfully!",
+                Data = model,
+            };
         }
     }
 }
--- Teslalab/Server/Controllers/PlaylistController.cs
+++ Teslalab/Server/Controllers/PlaylistController.cs
@@ -22,10 +22,37 @@ namespace Teslalab.Server.Controllers
             _playlistService = playlistService;
         }
 
+        [HttpGet("GetAll")]
+        public IActionResult GetAll(int pageNumber, int pageSize)
+        {
+            var result = _playlistService.GetAllPlaylists(pageNumber, pageSize);
+            return Ok(result);
+        }
+
         [HttpPost("Create")]
         public async Task<IActionResult> Create(PlaylistDto model)
         {
             var result = await _playlistService.CreateAsync(model);
+            if (result.IsSuccess)
+                return Ok(result);
+
+            return BadRequest(result);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(PlaylistDto model)
+        {
+            var result = await _playlistService.UpdateAsync(model);
+            if (result.IsSuccess)
+                return Ok(result);
+
+            return BadRequest(result);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _playlistService.RemoveAsync(id);
             if (result.IsSuccess)
                 return Ok(result);
 
